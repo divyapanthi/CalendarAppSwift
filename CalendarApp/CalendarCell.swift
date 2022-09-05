ca6c81e6fa8f243d378355bc766f621712f0ff4e
Divya <deevsofficial341@gmail.com>
2022-09-05 15:38:13 +0545

    first commit

---
+++ CalendarApp/CalendarCell.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarCell.swift
+//  CalendarApp
+//
+//  Created by Divya Panthi on 05/09/2022.
+//
+
+import UIKit
+
+class CalendarCell : UICollectionViewCell{
+    
+    @IBOutlet weak var dayOfMonth: UILabel!
+    
+    
+    
+}
+
---
+++ CalendarApp/CellCalendar/CollectionCalendarCell.swift
@@ -0,0 +1,19 @@
+//
+//  CollectionCalendarCell.swift
+//  CalendarApp
+//
+//  Created by Divya Panthi on 05/09/2022.
+//
+
+import UIKit
+
+class CollectionCalendarCell: UICollectionViewCell {
+
+    @IBOutlet weak var lblDateeee: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
